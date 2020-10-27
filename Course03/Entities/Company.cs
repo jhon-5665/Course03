8280bf70d3307584b487d1d1f6843fb5d3b0cf95
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-10-27 12:08:50 -0300

    Programa lendo dados de contribuintes

---
+++ Course03/Entities/Company.cs
@@ -0,0 +1,29 @@
+﻿namespace Course03.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company()
+        {
+        }
+        
+        public Company(string name, double anualIncome, int numberOfEmployees)
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberOfEmployees > 10.0)
+            {
+                return AnualIncome *= 0.14;
+            }
+            else
+            {
+                return AnualIncome *= 0.16;
+            }            
+        }
+    }
+}
---
+++ Course03/Entities/Individual.cs
@@ -0,0 +1,36 @@
+﻿namespace Course03.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual()
+        {
+        }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) 
+            : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.00)
+            {
+                AnualIncome *= 0.15;
+            }
+            else
+            {
+                AnualIncome *= 0.25;
+            }
+
+            if (HealthExpenditures > 0.0)
+            {
+                HealthExpenditures *= 0.50;
+            }
+
+            return AnualIncome - HealthExpenditures;
+        }
+    }
+}
